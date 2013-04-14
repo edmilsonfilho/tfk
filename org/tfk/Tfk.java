a9d5af411069fa851b9a5f9c6d9bca59b9ab581f
heliofrota <heliofrota@gmail.com>
2013-04-14 11:49:20 -0300

    Added wording copyright statement

--- org/tfk/Tfk.java
+++ org/tfk/Tfk.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright © 2013 Helio Frota <heliofrota@gmail.com>
+ * This work is free. You can redistribute it and/or modify it under the
+ * terms of the Do What The Fuck You Want To Public License, Version 2,
+ * as published by Sam Hocevar. See the COPYING file for more details.
+ */
 package org.tfk;
 
 /**
@@ -7,17 +13,18 @@ package org.tfk;
  * Edited on GNU/Emacs.
  */
 public class Tfk {
-        /**
-         * Método que inicializa o programa.
-         * Ele simplesmente imprime uma mensagem.
-         * @param String[] args
-        **/
-        public static void main(String[] args){
-                System.out.println("Ao infinito e além");
-                System.out.println("Aprendendo Git");
-                System.out.println("Javatfk");
-                System.out.println("FidiBequi");
-                System.out.println("Carinha feliz :)");
-        }
+
+    /**
+    * Método que inicializa o programa.
+    * Ele simplesmente imprime uma mensagem.
+    * @param String[] args
+    **/
+    public static void main(String[] args){
+        System.out.println("Ao infinito e além");
+        System.out.println("Aprendendo Git");
+        System.out.println("Javatfk");
+        System.out.println("FidiBequi");
+        System.out.println("Carinha feliz :)");
+    }
 
 }
