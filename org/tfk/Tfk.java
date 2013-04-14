ce9bebe52b1a34b3d68041bdc7fdb97ce8f24422
heliofrota <heliofrota@gmail.com>
2013-04-14 11:52:20 -0300

    changed @ from email to at

--- org/tfk/Tfk.java
+++ org/tfk/Tfk.java
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2013 Helio Frota <heliofrota@gmail.com>
+ * Copyright © 2013 Helio Frota <heliofrota at gmail.com>
  * This work is free. You can redistribute it and/or modify it under the
  * terms of the Do What The Fuck You Want To Public License, Version 2,
  * as published by Sam Hocevar. See the COPYING file for more details.
