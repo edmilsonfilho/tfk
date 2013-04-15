3cee84741df626d5727bc14ce7b3879c66ed529c
=jardel <jardelrodrigues93@gmail.com>
2013-04-15 16:02:37 -0300

    Corrigindo bug game

--- org/tfk/ui/Game.java
+++ org/tfk/ui/Game.java
@@ -43,7 +43,7 @@ public class Game {
         frame = new JFrame();
         frame.setTitle("TFK - The Fellowship of the Knowledge");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(800,600);
+        frame.setSize(400,350);
 
         //Menu Bar
         menuBar = new JMenuBar();
