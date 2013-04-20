553c507d53d93797d4afc5f6424d7afeaf5f2427
Levy Moreira <levymoreira.ce@gmail.com>
2013-04-20 12:58:08 -0300

    fixed #54

--- src/main/java/org/tfk/ui/Game.java
+++ src/main/java/org/tfk/ui/Game.java
@@ -54,7 +54,7 @@ public class Game {
 
         //Set Locale
         currentLocale = new Locale("pt", "BR"); //default bt_BR - Brazilian language
-        //Locale currentLocale = new Locale("en", "US"); //to set english language remove comment of this line and comment out the line above
+        //currentLocale = new Locale("en", "US"); //to set english language remove comment of this line and comment out the line above
         myResources = ResourceBundle.getBundle("org.tfk.i18n.messages", currentLocale);
 
         //Frame
@@ -62,10 +62,10 @@ public class Game {
         frame.setTitle("TFK - The Fellowship of the Knowledge");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(320, 370);
-	frame.setLocationRelativeTo(null);
+        frame.setLocationRelativeTo(null);
 
         //Layout
-        gridBagLayout = new GridBagLayout();       
+        gridBagLayout = new GridBagLayout();
         gridBagConstraints = new GridBagConstraints();
         gridBagConstraints.gridwidth = GridBagConstraints.REMAINDER; //end row
         gridBagConstraints.insets = new Insets(15,0,38,0) ; //spaces between buttons
@@ -83,20 +83,32 @@ public class Game {
         menuBar.add(menuLanguage);
         menuAlterLanguage = new JMenu(myResources.getString("menuAlterLanguage"));
         menuLanguage.add(menuAlterLanguage);
+
         menuItemEnUS = new JMenuItem("en-US");
-        menuItemPtBR = new JMenuItem("pt-BR");
         menuAlterLanguage.add(menuItemEnUS);
+        menuItemEnUS.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent actionEvent){
+                changeLanguage("en", "US");
+            }
+        });  
+
+        menuItemPtBR = new JMenuItem("pt-BR");
         menuAlterLanguage.add(menuItemPtBR);
+        menuItemPtBR.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent actionEvent){
+                changeLanguage("pt", "BR");
+            }
+        }); 
 
         menuOptions = new JMenu(myResources.getString("menuOptions"));
         menuBar.add(menuOptions);
-        JMenuItem menuItemExit = new JMenuItem(myResources.getString("menuItemExit"));
+        menuItemExit = new JMenuItem(myResources.getString("menuItemExit"));
         menuOptions.add(menuItemExit);
         menuItemExit.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent actionEvent){
                 System.exit(0);
             }
-        });       
+        });
 
         //Buttons
         buttonStart = new JButton(myResources.getString("buttonStart"));
@@ -112,32 +124,43 @@ public class Game {
             public void actionPerformed(ActionEvent actionEvent){
                 new DialogCredits().initComponents();
             }
-        });    
+        });
         gridBagLayout.setConstraints(buttonCredits, gridBagConstraints);
-        panel.add(buttonCredits);
-        
+
+        panel.add(buttonCredits);        
         frame.add(panel);
         frame.setVisible(true);
     }
 
+    private void changeLanguage(String language, String country){
+        currentLocale = new Locale(language, country);
+        myResources = ResourceBundle.getBundle("org.tfk.i18n.messages", currentLocale);
+        buttonStart.setText(myResources.getString("buttonStart"));
+        buttonOptions.setText(myResources.getString("buttonOptions"));
+        buttonCredits.setText(myResources.getString("buttonCredits"));
+        menuLanguage.setText(myResources.getString("menuLanguage"));
+        menuAlterLanguage.setText(myResources.getString("menuAlterLanguage"));
+        menuOptions.setText(myResources.getString("menuOptions"));
+        menuItemExit.setText(myResources.getString("menuItemExit"));
+    }
+
     private class DialogCredits extends JDialog{
-        public DialogCredits(){}
-
-        public void initComponents() {            
-            JPanel panelCredits = new JPanel();
-            panel.setLayout(new GridBagLayout());
-            panelCredits.add(new JLabel("Helio Frota https://github.com/heliofrota"));    
-            panelCredits.add(new JLabel("Levy Moreira https://github.com/levymoreira"));  
-            panelCredits.add(new JLabel("Leandro Nascimento https://github.com/LeandroNascimento"));  
-            panelCredits.add(new JLabel("Manoel Calixto https://github.com/manoelcalixto")); 
-	    panelCredits.add(new JLabel("Maciel Melo https://github.com/MacielMelo"));
-            add(panelCredits);
-	    setSize(450, 140);
+        public DialogCredits(){
+            this.setSize(450, 140);
+            this.setTitle(myResources.getString("buttonCredits"));
             this.setLocationRelativeTo(null);
-	    setTitle(myResources.getString("buttonCredits"));
-            setVisible(true);
-	    			
+        }
 
+        public void initComponents() {
+            JPanel panelCredits = new JPanel();
+            panelCredits.add(new JLabel("Helio Frota https://github.com/heliofrota"));
+            panelCredits.add(new JLabel("Levy Moreira https://github.com/levymoreira"));
+            panelCredits.add(new JLabel("Leandro Nascimento https://github.com/LeandroNascimento"));
+            panelCredits.add(new JLabel("Manoel Calixto https://github.com/manoelcalixto")); 
+            panelCredits.add(new JLabel("Maciel Melo https://github.com/MacielMelo"));
+
+            this.add(panelCredits);
+            this.setVisible(true);
         }
     }
 
